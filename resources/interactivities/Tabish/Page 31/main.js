b802e63161aa5b80ef8139232b73ca1a010479b3
tabishtanseef <tabishtanseef@gmail.com>
2023-05-08 00:09:45 +0530

    Added typing activity page 25 and 29 30 31 click on sentence

---
+++ resources/interactivities/Tabish/Page 31/main.js
@@ -0,0 +1,96 @@
+var q=0;
+var r=0;
+var s=0;
+var t=0;
+
+function check(){
+	if(q==1){
+		$(".t_1").removeClass('hidden');
+		$(".c_1").addClass('hidden');
+		bo(1);
+	}else{
+		$(".c_1").removeClass('hidden');
+		$(".t_1").addClass('hidden');
+	}
+	if(r==1){
+		$(".t_2").removeClass('hidden');
+		$(".c_2").addClass('hidden');
+		bo(2);
+	}else{
+		$(".c_2").removeClass('hidden');
+		$(".t_2").addClass('hidden');
+	}
+	if(q==1 && r==1){
+		$("#sol").css("pointer-events", "auto");
+		var audio = document.getElementById('audio1');
+		audio.play();
+		setTimeout(function(){ 
+		 $('#blocks').show(); 
+		},500);
+	}else{
+		var audio = document.getElementById('audio2');
+		audio.play();
+		setTimeout(function(){ 
+			$('#blocks2').show();
+		},1000);
+	}
+}
+
+function play(){
+	var audio = document.getElementById('audio3');
+	audio.play();
+}
+
+function solve(){
+	a=1;
+	while(a<3){
+		$(".c_"+a).addClass('hidden');
+		//$(".t_"+a).removeClass('hidden');
+		b = a+10;
+		c = b+100;
+		$('#'+c).css('color','black');
+		$('#'+b).css('color','black');
+		$('#'+a).css('color','white');
+		$('#'+a).css('background','green');
+		a++;
+	}
+	$('#blocks2').hide();
+	$('#submit').css('pointer-events','none');
+	$('#submit').css('opacity','0.5');
+}
+function go(a){
+	$('#'+a).css('background','#FECA0A');
+	$('#'+a).css('pointer-events','none');
+	if(a==1 || a==2){
+		b = parseFloat(a)+10;
+		c = parseFloat(b)+100;
+		$('#'+b).css('pointer-events','none');
+		$('#'+c).css('pointer-events','none');
+	}else if(a>100){
+		b = parseFloat(a)-100;
+		c = parseFloat(b)-10;
+		$('#'+b).css('pointer-events','none');
+		$('#'+c).css('pointer-events','none');
+	}else{
+		b = parseFloat(a)+100;
+		c = parseFloat(a)-10;
+		$('#'+b).css('pointer-events','none');
+		$('#'+c).css('pointer-events','none');
+	}
+	
+	$('#'+a).css('pointer-events','none');
+	if(a==1){
+		q++;
+	}
+	if(a==2){
+		r++;
+	}
+}
+function bo(a){
+	b = a+10;
+	c = b+100;
+	$('#'+c).css('color','black');
+	$('#'+b).css('color','black');
+	$('#'+a).css('color','green');
+	$('#'+a).css('border','5px solid green');
+}
